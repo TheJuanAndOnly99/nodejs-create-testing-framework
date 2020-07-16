60a9ad3bfa9d8736b54f9867959821d0c8057ff3
Juan Estrella <juan.estrella137@gmail.com>
2020-07-16 16:35:28 +0200

    Bug fix- add script execution delay

--- render.js
+++ render.js
@@ -4,12 +4,17 @@ const { JSDOM } = jsdom;
 
 const render = async (filename) => {
   const filePath = path.join(process.cwd(), filename);
+
   const dom = await JSDOM.fromFile(filePath, {
     runScripts : 'dangerously',
     resources  : 'usable'
   });
 
-  return dom;
+  return new Promise((resolve, reject) => {
+    dom.window.document.addEventListener('DOMContentLoaded', () => {
+      resolve(dom);
+    });
+  });
 };
 
 module.exports = render;
