f4e65913c091b1fc3219aff316d27879ec4d0d24
Juan Estrella <juan.estrella137@gmail.com>
2020-07-16 15:20:46 +0200

    Implement error handling functionality in runTests function

--- runner.js
+++ runner.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const path = require('path');
+const chalk = require('chalk');
+
+const forbiddenDirs = [ 'node_modules' ];
 
 class Runner {
   constructor() {
@@ -8,15 +11,28 @@ class Runner {
 
   async runTests() {
     for (let file of this.testFiles) {
+      console.log(chalk.grey(`---- ${file.shortName}`));
       const beforeEaches = [];
       global.beforeEach = (fn) => {
         beforeEaches.push(fn);
       };
       global.it = (desc, fn) => {
         beforeEaches.forEach((func) => func());
-        fn();
+        try {
+          fn();
+          console.log(chalk.green(`\tOK - ${desc}`));
+        } catch (err) {
+          const message = err.message.replace(/\n/g, '\n\t\t');
+          console.log(chalk.red(`\tX - ${desc}`));
+          console.log(chalk.red('\n', '\t', message));
+        }
       };
-      require(file.name);
+
+      try {
+        require(file.name);
+      } catch (err) {
+        console.log(chalk.red(err));
+      }
     }
   }
 
@@ -28,9 +44,9 @@ class Runner {
       const stats = await fs.promises.lstat(filepath);
 
       if (stats.isFile() && file.includes('.test.js')) {
-        this.testFiles.push({ name: filepath });
+        this.testFiles.push({ name: filepath, shortName: file });
       }
-      else if (stats.isDirectory()) {
+      else if (stats.isDirectory() && !forbiddenDirs.includes(file)) {
         const childFiles = await fs.promises.readdir(filepath);
 
         files.push(...childFiles.map((f) => path.join(file, f)));
