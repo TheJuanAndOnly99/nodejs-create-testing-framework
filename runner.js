c4b31c1f0dcb08ad5cab8e8ad2492da0ea868668
Juan Estrella <juan.estrella137@gmail.com>
2020-07-16 13:02:15 +0200

    Import nodejs fs module, create async function collectFiles that returns a list of all files inside target path

--- runner.js
+++ runner.js
@@ -1,3 +1,15 @@
-class Runner {}
+const fs = require('fs');
+
+class Runner {
+  consructor() {
+    this.files = [];
+  }
+
+  async collectFiles(targetPath) {
+    const files = await fs.promises.readdir(targetPath);
+
+    return files;
+  }
+}
 
 module.exports = Runner;
